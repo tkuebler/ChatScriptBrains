c0202cb949a0b3ae6cfec1447b3b9ad6f1d9da13
Todd Kuebler <todd@kuebler.org>
2014-05-01 16:06:13 -0700

    partially through ai..aiml  - stopping for the day - improvements to aiml2cs.pl detects bot, deep nested content, lists of random responses still needs lots of work in the bot, think, sria, and some others trying to detect and comment out non-valid cs so that until deeper support is added the files can still be used.

--- aiml2cs/aiml2cs.pl
+++ aiml2cs/aiml2cs.pl
@@ -33,22 +33,70 @@ print "processing $_...\n";
 
 my $aiml = XMLin($_filename);
 
+print Dumper($aiml);
+
 my $out = "TOPIC: ~$topic-aiml ()\n\n";
 
 foreach my $category (@{$aiml->{category}})
 {
-	my $pattern = $test = lc $category->{pattern};
-	$test =~ s/\*//g;
-	$test =~ s/ +/ /g;
-	if ( $category->{template}->{srai} ) 
+# 	http://www.chatbots.org/ai_zone/viewthread/1466/
+# 	
+# 	1. convert all upper case to lower case—this will be flawed for words which are actually proper names
+# 	2. convert the AIML pattern of   word * word * word to u: (< word * word >)
+# 	3. convert the AIML pattern of * word * word * word to u: ( word * word * word >)
+# 	and so on, for all the sentence boundary forms.
+# 	4. for aiml output that is just words, put them on the chatscript output side.
+# 	5. to map other functions of aiml output, you have to handle that on a function decode by function decode basis.
+
+	my $pattern = lc $category->{pattern};
+	my $test = $pattern;
+	$test =~ s/\*/TEST/g;
+	$pattern =~ s/(.*)\*(.*)\*(.*)/<$1*$2*$3>/g; 		# 2
+	$pattern =~ s/(.*)\*(.*)\*(.*)\*/<$1*$2*$3*/g;		# 3
+	$pattern =~ s/\*(.*)\*(.*)\*(.*)/*$1*$2*$3*>/g;		# 3
+	$pattern =~ s/(.*)\*(.*)/<$1*$2>/g; 		# 2
+	$pattern =~ s/(.*)\*(.*)\*/<$1*$2*/g;		# 3
+	$pattern =~ s/\*(.*)\*(.*)/*$1*$2>/g;		# 3
+	$pattern =~ s/ +/ /g;
+
+	if ( $category->{template}->{bot} ) 
 	{
 		$out .= "#! $test\n";
-		$out .=  "u: ($pattern) \^reuse($category->{template}->{srai})\n";
+		$out .= "u: \($pattern\) $category->{template}->{bot}->{name}\n";
+	}
+	elsif ( $category->{template}->{srai} ) 
+	{
+		$out .= "#! $test\n";
+		if ( $category->{template}->{srai}->{content} ) 
+		{
+			$out .= "u: ($pattern) \^reuse($category->{template}->{srai}->{content})\n";
+		}
+		else
+		{
+			$out .= "u: ($pattern) \^reuse($category->{template}->{srai})\n";
+		}
+	}
+	elsif ( $category->{template}->{random} )
+	{
+		$out .= "#! $test\n";
+		$out .= "u: ($pattern) ";
+		foreach my $li ( @{$category->{template}->{random}->{li}})
+		{
+			if ( ref($li) eq "STRING")
+			{
+				$out .= "[$li]\n";	
+			} 
+			else
+			{
+				$out .= "# $li \n";
+			}
+		}
+		$out .= "\n"
 	}
 	elsif ( $category->{template}->{content} )
 	{
 		$ref = ref($category->{template}->{content});
-		if ( $ref eq "ARRAY")
+		if ( $ref ne "STRING" )
 		{
 			$out .= "#! $test\n";
 			$out .= "u: \($pattern\) $category->{template}->{content}[0]\n";
